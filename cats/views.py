739558d3e757e6f2091d30835d3f9283feb086ee
Ivan Kobzev <iam@caseynger.ru>
2025-03-24 12:39:48 +0300

    code refactoring with lowlevel CBV

--- cats/views.py
+++ cats/views.py
@@ -1,45 +1,22 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        # Создаем объект сериализатора и передаем в него данные
-        # из POST-запроса
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
         serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
-            # Если полученные данные валидны - сохранем в БД
             serializer.save()
-            # Возвращаем JSON со всеми данными нового объекта
-            # и статус-код 201
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # Если данные не валидны - возвращаем информацию об ошибках
-        # и статус-код
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    # Получаем все объекты модели в случае GET-запроса
-    cats = Cat.objects.all()
-    # Чтобы сериализатор был готов принять список объектов -
-    # передаем именнованный параметр "many=True"
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+    
 
-
-@api_view(['GET', 'POST'])
-def hello(request):
-    if request.method == 'POST':
-        return Response(
-            {
-                'message': 'Получены данные',
-                'data': request.data
-            }
-        )
-    return Response(
-        {
-            'message': 'Это был GET-запрос!'
-        }
-    )
